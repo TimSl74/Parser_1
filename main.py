dbec707077fdc22b042a787bb723ee8885c72a1f
Viacheslav <tayter74@mail.ru>
2023-01-30 22:48:29 +0500

    Настроил поиск ссылок по предметам. Выводится в print

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import requests as r
 from bs4 import BeautifulSoup
 
-url = "https://gdz-shok.ru/gdz/klass_1/matematika/moro_volkova/1.3/"
+url = "https://gdz-shok.ru"
 
-response = r.get(url)
+response = r.get(url).text
+soup = BeautifulSoup(response, "lxml")
 
-soup = BeautifulSoup(response.text, "lxml")
-data = soup.find("div", class_="ex")
-pic = data.find("img", class_="ex").get("src")
-
-print(data, "\n")
-print(pic)
+datas = soup.find_all("div", class_="td")
 
 with open("soup.json", "w") as file:
-    file.write(soup.text)
+    file.write(str(soup))
+
+with open("datas.json", "w") as file:
+    file.write(str(datas))
+
+for data in datas:
+    try:
+        url_2 = data.find("a").get("href")
+        if url_2 != "#":
+            if len(url_2) > 14:  # 14 - длина строки url_2, в которой нет ссылки на предмет, только на класс
+                print(f"{url}{url_2}")
+            else:
+                pass
+        else:
+            pass
+    except:
+        pass
+# Вместо print нужно собирать все ссылки в список, который потом уже перебирать запросами
+# В списке нужно убрать дублирующиейся ссылки, потому что ссылки на 1 класс дублируются
