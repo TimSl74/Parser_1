9d411a1f8cb2124498b334eacea894e2fb21c891
Viacheslav <tayter74@mail.ru>
2023-01-31 14:51:23 +0500

    Настроил сохранение всех ссылок по предметам в json файлы

--- main.py
+++ main.py
@@ -1,30 +1,60 @@
 import requests as r
 from bs4 import BeautifulSoup
 
+
+# Обработка стартовой страницы
 url = "https://gdz-shok.ru"
+
 
 response = r.get(url).text
 soup = BeautifulSoup(response, "lxml")
-
 datas = soup.find_all("div", class_="td")
 
+
+# Сохранение промежуточных запросов в файлы json для проверки
 with open("soup.json", "w") as file:
     file.write(str(soup))
 
 with open("datas.json", "w") as file:
     file.write(str(datas))
 
+
+data_list = []
+
 for data in datas:
     try:
         url_2 = data.find("a").get("href")
         if url_2 != "#":
             if len(url_2) > 14:  # 14 - длина строки url_2, в которой нет ссылки на предмет, только на класс
-                print(f"{url}{url_2}")
+                data_list.append(f"{url}{url_2}")
+                # print(f"{url}{url_2}")
             else:
                 pass
         else:
             pass
     except:
         pass
-# Вместо print нужно собирать все ссылки в список, который потом уже перебирать запросами
-# В списке нужно убрать дублирующиейся ссылки, потому что ссылки на 1 класс дублируются
+
+
+# Убираем дублирующиеся элементы
+data_list_2 = []
+for data in data_list:
+    if data not in data_list_2:
+        data_list_2.append(data)
+
+data_list = data_list_2
+"""
+for data in data_list:
+    print(data)
+"""
+
+# Перебираем ссылки из data_list
+n = 0
+while n < len(data_list)-1:
+    response = r.get(data_list[n]).text
+    soup = BeautifulSoup(response, "lxml")
+    with open(f"{str((data_list[n])[24::]).replace('/','_')}.json", "w") as file:
+        file.write(str(soup))
+    n += 1
+
+# Нужно настроить запрос по ссылкам в сохраняемых json файлах
