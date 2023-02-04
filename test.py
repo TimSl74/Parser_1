bd8204f4671c6dcfb2adb31b694d02d0b0f71bfa
Windows <tayter74@mail.ru>
2023-02-04 11:35:57 +0500

    test commit from windows

--- test.py
+++ test.py
@@ -119,5 +119,3 @@ print("control point 7")
     # line = f"/Users/macbook/PycharmProjects/Parser_1/Images/{name}"
     # Путь для Windows
     # line = f"C:/Users/Tayte/PycharmProjects/Parser_1/Images/{name}"
-
-# test commit from windows
